09e895bbe3c86f604f43b2a135703fde393f6c13
AndresPorrasDev <138604800+andresporrasdev@users.noreply.github.com>
2024-11-10 20:40:03 -0500

    Up and down items functions implemented

--- TodoAppAndres/src/App.jsx
+++ TodoAppAndres/src/App.jsx
@@ -4,18 +4,13 @@ import './App.css'
 import { v4 as uuid} from "uuid";
 import PropTypes from 'prop-types';
 
-
 function App() {
   const [items, setItems] = useState([
     { id: 1, content: "pay bills", done: true },
     { id: 2, content: "learn React", done: false },
   ]);
   const [input, setInput] = useState("")
-
-  // const handleOnClick = task => {
-  //   console.log("add this task", task.target.value)
-  // }
-  
+ 
   const handleOnChange = e => {
     setInput(e.target.value)
     console.log(input)
@@ -33,12 +28,38 @@ function App() {
     console.log("Deleted item with the id", id)
   }
 
+  const taskDown = id => {
+    const index = items.findIndex(item => item.id === id);
+    if (index === items.length - 1) return;
+    else {
+      const updatedItems = [...items];
+      const previousIndex = updatedItems[index];
+      const nextIndex = updatedItems[index+1];
+      updatedItems[index+1] = previousIndex;
+      updatedItems[index] = nextIndex;
+      setItems(updatedItems);
+    }
+  }
+
+  const taskUp = id => {
+    const index = items.findIndex(item => item.id === id);
+    if (index === 0) return;
+    const updatedItems = [...items];
+    const previousIndex = updatedItems[index];
+    const nextIndex = updatedItems[index-1];
+    updatedItems[index-1] = previousIndex;
+    updatedItems[index] = nextIndex;
+    setItems(updatedItems);
+  }
+
   function Item ({id, content, done, onDelete}) {
     return(
       <li key={id}>
         <input className="form-check-input" type="checkbox" aria-label="..."/>
         <span className={done ? "done" : ""}>{content}</span>
         <button className='btn btn-danger btn-sm' onClick={()=>onDelete(id)}>Delete</button>
+        <button className='btn btn-info btn-sm'onClick={()=>taskUp(id)}>up</button>
+        <button className='btn btn-info btn-sm' onClick={()=>taskDown(id)}>down</button>
       </li>
     )
   };
